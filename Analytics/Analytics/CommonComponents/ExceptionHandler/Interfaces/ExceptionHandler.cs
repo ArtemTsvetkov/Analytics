df334d6f0e0f5895d5a5ea45050afefdf95781c0
TsvetkovArtem <artem.tsvetkov.95@bk.ru>
2018-07-01 22:46:14 +0300

    add new interfaces dor exeption handler

---
+++ Analytics/Analytics/CommonComponents/ExceptionHandler/Interfaces/ExceptionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analytics.CommonComponents.ExceptionHandler.Interfaces
+{
+    interface ExceptionHandler
+    {
+        //Обработка исключения
+        void processing(Exception exception);
+        //Добавление нового исключения
+        void addException(ConcreteException exception);
+    }
+}
---
+++ Analytics/Analytics/CommonComponents/ExceptionHandler/Interfaces/ExceptionView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analytics.CommonComponents.ExceptionHandler.Interfaces
+{
+    interface ExceptionView<TConfigType>
+    {
+        //Установка конфигурации
+        void setConfig(TConfigType config);
+        //Отображение вью
+        void show();
+    }
+}
